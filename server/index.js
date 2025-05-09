9d9850ebc764c90a48d1033c336ed132834793c3
Benjamin Panitz <panitz.benjamin@gmail.com>
2025-05-09 20:17:24 +0200

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Beispielroute
+app.get('/', (req, res) => {
+  res.send('Hell from backend!');
+});
+
+// MongoDB Verbindung
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server läuft auf Port ${PORT}`)))
+  .catch(err => console.error(err));
